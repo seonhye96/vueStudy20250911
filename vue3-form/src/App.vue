cc45d2a42c537207533b9df47df0ab04b264872a
seonhye96 <ghkdtjs6451@gmail.com>
2025-09-19 15:19:47 +0900

    vue3 시작하기 완

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID:</label>
+      <input id="userId" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit" @click="logText">로그인</button>
+  </form>
+</template>
+
+<script>
+  import axios from 'axios'
+import { ref } from 'vue';
+
+  export default {
+    setup(){
+      // data 선언
+      var username = ref('');
+      var password = ref('');
+
+      // methods 선언
+      var submitForm = () => {
+        axios.post('https://jsonplaceholder.typicode.com/users/', {
+          username: username.value,
+          password: password.value,
+        }).then(response => {
+          console.log(response);
+        })
+      }
+
+      return {username, password, submitForm}
+    }
+
+    // data() {
+    //   return {
+    //     username: '',
+    //     password: ''
+    //   }
+    // },
+    // methods:{
+    //   submitForm(){
+    //     // event.preventDefault();
+    //     const data = {
+    //       username: this.username,
+    //       password: this.password,
+    //     }
+
+    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
+    //       .then(response => {
+    //         console.log(response);
+    //       });
+    //     // console.log('제출됨');
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+
+</style>
