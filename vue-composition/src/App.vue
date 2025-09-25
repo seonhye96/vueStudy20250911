bd1af4643699c2934eff1dbb5f90354581ddd653
seonhye96 <ghkdtjs6451@gmail.com>
2025-09-25 14:15:01 +0900

    vue3 중급 - Compositon API > ~섹션1

---
+++ vue-composition/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">chage</button>
+  </div>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import { useMessage } from './hooks/useMessage'
+
+  export default {
+    /** step1 */
+    // data() {
+    //   return {
+    //     message: 'hello'
+    //   }
+    // },
+    // methods: {
+    //   changeMessage(){
+    //     this.message='hi'
+    //   }
+    // }
+
+    /** step2 */
+    setup(){
+        /** step3 */
+        const { message , changeMessage } = useMessage();
+      // // data
+      // const message = ref('hello');
+
+      // //methods
+      // function changeMessage(){
+      //   message.value = 'hi';
+      // }
+
+      return {message , changeMessage}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
